0dc11e25770719642561b6ced164bda1668e8a6a
Caleb Lazarow <55657156+clazarow1@users.noreply.github.com>
2024-04-12 09:54:25 -0500

    Create print.py
    Print image for the CSP CPT project

---
+++ print.py
@@ -0,0 +1,39 @@
+print("""\n
+
+
+
+
+
+# Original author: https://github.com/notnop
+# Original Modifier for the project: Endermanch (https://github.com/Endermanch/MalwareDatabase/blob/master/ddom.py)
+# Originally modified for Python 3
+# This project is inspired by Endermanch, check him out here:
+# https://youtube.com/endermanch
+#
+#
+#                                                                                
+#                                  @@         @&                                 
+#                              @@@               @@@                             
+#                         ,@@@@@                   @@@@@.                        
+#                       @@@@@                        .@@@@#                      
+#                       @@@@@,                       /@@@@@                      
+#                       @@@@@@                       @@@@@@                      Malware Samples downloaded to your computer!
+#                       @@@@@@          /@,          @@@@@@                      
+#                       @@@@@@      @@@@@@@@@@@      @@@@@@                      Please take responsibility when downloading these samples,
+#                       @@@@@@# @@@@@@@.   (@@@@@@@ @@@@@@@                      as these samples can damage your host system.
+#                      @@@@@@@@@@@               @@@@@@@@@@@                     
+#                  @@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@@                 I recommend using Linux for downloading samples, and not
+#              &@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@%             executing these on your main system... unless you know
+#          %@@@@@@@@@@@,  . @@@@@@@#           @@@@@@@@ %  ,@@@@@@@@@@@(         what you're doing.
+#         @@@@@@@(        @@@@ &@@@@@         @@@@@( @@@@        (@@@@@@@        
+#         @@@@            @@@@     @  *     &  @     @@@@            @@@@        Thank you in advance!
+#         @@@             @@@@      @@@@   @@@@      @@@@             @@@        
+#         @@              @@@@@%    @@@@@#@@@@@    @@@@@@             .@@        
+#         @&                %@@@@@@*#@@@@@@@@@ @@@@@@@(                @@        
+#         @                     .@@@ @@@@@@@@@ @@@                      @        
+#                                    @@@@@@@@@                                   
+#                                %@@@@@@@@@@@@@@@,                               
+#             @.              @@@@@@@@@@@@@@@@@@@@@@@              *@            
+#                 %@@@@@& ,@@@@@@@@@@        ,@@@@@@@@@@ .@@@@@@(                
+#                      @@@@@@@@@*                 #@@@@@@@@@                     
+#                          &                           &                       \n""")
